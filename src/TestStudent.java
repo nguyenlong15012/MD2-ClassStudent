ab8d9f1810636206cb1a313875a518be43a3520d
Nguyen Long <nguyenlong232253@gmail.com>
2022-03-25 14:36:18 +0700

    nopbaitap

---
+++ src/TestStudent.java
@@ -0,0 +1,9 @@
+public class TestStudent {
+    public static void main(String[] args) {
+        Student stu = new Student();
+        stu.setName("Trang");
+        stu.setClasses("KT01");
+
+        System.out.println("Name: " + stu.getName() + " Class: " + stu.getClasses());
+    }
+}
